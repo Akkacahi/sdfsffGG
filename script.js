2fb31f6d80ef035a3fce3cb301f972b9404b0e00
Akkacahi <90656417+Akkacahi@users.noreply.github.com>
2021-12-01 17:31:22 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,105 @@
+function contactsCtrl($scope) {
+
+  $scope.contacts = [
+    {
+      name:"",
+      code:"",
+      year:"",
+      img:"",
+    },{
+      name:"",
+      code:"",
+      year:"",
+      img:"",
+    },{
+      name:"",
+      code:"",
+      year:"",
+      img:"",
+    },{
+      name:"",
+      code:"",
+      year:"",
+      img:"",
+    },
+    
+    
+    
+    {
+      name:"",
+      code:"",
+      year:"",
+      img:"",
+    },{
+      name:"เห็นด้วยกับการฉีดวัคซีนไขว้กันหรือไม่",
+      code:"",
+      year:"",
+      img:"https://images.unsplash.com/photo-1623682687826-fe06bf64e6d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fHZhY2NpbmV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    },{
+      name:"เห็นด้วยกับการที่เด็กรุ่นใหม่ย้ายประเทศหรือไม่ ",
+      code:"",
+      year:"",
+      img:"https://images.unsplash.com/photo-1613018274564-bec35a774995?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDV8fGFpcnBvcnR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+    name:"เห็นด้วยกับการทำประกันให้ ไรเดอร์หรือไม่ ",
+    code:"",
+    year:"",
+    img:"https://assets.change.org/photos/4/id/zk/ltIdzKQxndnzzHl-800x450-noPad.jpg?1631284823",
+  },
+  {
+    name:"เห็นด้วยกับการปฏิรูปการศึกษาหรือไม่ ",
+    code:"",
+    year:"",
+    img:"https://www.matichon.co.th/wp-content/uploads/2017/06/%E0%B8%A0%E0%B8%9B-%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B8%A8%E0%B8%B6%E0%B8%81%E0%B8%A9%E0%B8%B2.jpg",
+  },
+  {
+    name:"เห็นด้วยกับการ จัดอุปกรณ์พร้อมอินเตอร์เน็ตคุณภาพดี ",
+    code:"",
+    year:"",
+    img:"https://dokkooon.com/wp-content/uploads/2017/12/office-serive.jpg",
+  },
+  {
+    name:"เห็นด้วยกับการให้คะเเนนนักศึกษา จากยอกไลค์ยอกเเชรหรือไม่ ",
+    code:"",
+    year:"",
+    img:"https://assets.change.org/photos/2/fx/am/DTFXamFSRAsDIeb-800x450-noPad.jpg?1614662346",
+  },
+  {
+    name:"The Amazing Spider Man 3?. ",
+    code:"",
+    year:"",
+    img:"https://www.prachachat.net/wp-content/uploads/2019/08/spider-man-far-from-home.jpg",
+  },
+  {
+    name:"Ralf Rangnick มา Man U!!?? ",
+    code:"",
+    year:"",
+    img:"https://phantom-marca.unidadeditorial.es/66b0cc8983439b3bb05e6ddfe956d3d1/crop/0x26/2048x1176/resize/1320/f/jpg/assets/multimedia/imagenes/2021/11/25/16378644007639.jpg",
+  },
+   {
+    name:"นายก ประยัทธ์ จั้นทรา ควรลาออก!!!",
+    code:"",
+    year:"",
+    img:"https://f.ptcdn.info/465/068/000/q6x1o2caNC9dfYHT6O-o.jpg",
+  },
+  {
+    name:"ความเห็นต่อค่านิยมการบอกรัก/แสดงความรักในวันวาเลนไทน์",
+    code:"",
+    year:"",
+    img:"https://s.isanook.com/ca/0/rp/r/w850/ya0xa0m1w0/aHR0cHM6Ly9zLmlzYW5vb2suY29tL2NhLzAvdWQvMTg2LzkzMTgyMi92YWxlbnRpbmVzLWRheS5qcGc=.jpg",
+  },{
+    name:"คุณเคยติดโควิดหรือไม่",
+    code:"",
+    year:"",
+    img:"https://images.unsplash.com/photo-1623438787452-56beec0c9d62?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzZ8fGNvdmlkJTIwMTl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+  },{
+    name:"ฉีดวัคซีนแล้วหรือไม่",
+    code:"",
+    year:"",
+    img:"https://media.istockphoto.com/photos/mature-man-taking-a-vaccine-from-his-doctor-picture-id1266494637?b=1&k=20&m=1266494637&s=170667a&w=0&h=EfXVy9i-fKkPBl5vX8D1JNfXXw_EQySze7CMQUqSkik=",
+  },
+    
+  ]
+
+}
